48736fe858e942cb3c20dee458bd58eaa533e6b3
bebeehi <87840441+bebeehi@users.noreply.github.com>
2021-07-23 08:45:09 +0000

    Add files via upload

--- IdeaProjects/bee-demo/src/main/java/MyRange.java
+++ IdeaProjects/bee-demo/src/main/java/MyRange.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class MyRange {
     private final String input;
 
@@ -6,13 +8,43 @@ public class MyRange {
     }
 
     public int getStart() {
+        int result = 0;
         if(isStartWithInclude()) {
-            return Integer.parseInt(String.valueOf(this.input.charAt(1)));
+            result = Integer.parseInt(String.valueOf(this.input.charAt(1)));
+        } else {
+            result = Integer.parseInt(String.valueOf(this.input.charAt(1))) + 1;
         }
-        return -1;
+        return result;
     }
 
     public boolean isStartWithInclude() {
         return this.input.startsWith("[");
+    }
+
+    public boolean isEndWithInclude() {
+        return this.input.endsWith("]");
+    }
+
+    public int getEnd() {
+        if(isEndWithInclude()) {
+            return this.input.charAt(3) - 48;
+        }
+        return this.input.charAt(3) - 49;
+    }
+
+    public String getResult() {
+        int stratNumber = getStart();
+        int endNumber = getEnd();
+        String response = new String();
+        int firstLoop = 1;
+        for (int i = stratNumber; i <= endNumber; i++) {
+            if(firstLoop > 1) {
+                response = response + "," + (String.valueOf(i));
+            } else {
+                response = String.valueOf(i);
+            }
+            firstLoop++;
+        }
+        return response;
     }
 }
